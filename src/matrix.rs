f854f23ce44ae0646de8f57b8c2fc9a1b5b36167
Sebastien Boisvert <sebhtml@protonmail.com>
2024-03-01 18:07:05 +0100

    feat: implement matrix multiplication

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,16 +1,79 @@
+use std::ops::Mul;
+
+#[derive(Debug, PartialEq)]
 pub struct Matrix {
-    rows: u32,
-    cols: u32,
-    cells: Vec<f32>,
+    rows: usize,
+    cols: usize,
+    values: Vec<f32>,
 }
 
 impl Matrix {
-    pub fn new(rows: u32, cols: u32, cells: Vec<f32>) -> Self {
-        Self { rows, cols, cells }
+    pub fn new(rows: usize, cols: usize, values: Vec<f32>) -> Self {
+        Self { rows, cols, values }
     }
 
-    pub fn shape(&self) -> (u32, u32) {
+    pub fn shape(&self) -> (usize, usize) {
         (self.rows, self.cols)
+    }
+}
+
+impl Mul for &Matrix {
+    type Output = Matrix;
+
+    fn mul(self, rhs: &Matrix) -> Self::Output {
+        let lhs: &Matrix = self;
+        let (lhs_rows, lhs_cols) = lhs.shape();
+        let (rhs_rows, rhs_cols) = rhs.shape();
+        let (output_rows, output_cols) = (rhs_rows, lhs_cols);
+        let mut values = Vec::new();
+        values.resize(output_rows * output_cols, 0.0);
+
+        /*
+        Example:
+
+                  0 1
+              0 | a b |
+        lhs = 1 | c d |
+              2 | e f |
+
+                  0 1 2
+              0 | g h i |
+        rhs = 1 | k l m |
+
+                         0                    1              <- column in lhs
+        output = 0 | a*g + c*h + e*i     b*g + d*h + f*i |
+                 1 | a*k + c*l + e*m     b*k + d*l + f*m |
+
+                 ^
+                 |
+                 row in rhs
+
+        For cell (row, col) in output, we need:
+            - the row "row" in rhs
+            - the column "col" in lhs.
+         */
+
+        let mut output = Matrix::new(output_rows, output_cols, values);
+        let mut output_index: usize = 0;
+        let output_stride = 1;
+        let lhs_stride = lhs_cols;
+        let rhs_stride = 1;
+
+        for row in 0..output_rows {
+            for col in 0..output_cols {
+                let mut lhs_index = col;
+                let mut rhs_index = row * rhs_cols;
+                let mut output_value = 0.0;
+                for _ in 0..lhs_rows {
+                    output_value += lhs.values[lhs_index] * rhs.values[rhs_index];
+                    lhs_index += lhs_stride;
+                    rhs_index += rhs_stride;
+                }
+                output.values[output_index] = output_value;
+                output_index += output_stride;
+            }
+        }
+        output
     }
 }
 
@@ -19,7 +82,7 @@ mod tests {
     use crate::matrix::Matrix;
 
     #[test]
-    fn constructor_test() {
+    fn new() {
         // Given rows and cols
         // When a matrix is built
         // Then it has the appropriate shape
@@ -27,8 +90,57 @@ mod tests {
         let matrix = Matrix::new(
             4,
             3,
-            vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            vec![
+                //
+                0.0, 0.0, 0.0, //
+                0.0, 0.0, 0.0, //
+                0.0, 0.0, 0.0, //
+                0.0, 0.0, 0.0, //
+            ],
         );
         assert_eq!(matrix.shape(), (4, 3));
+    }
+
+    // TODO add a test for incorrect shapes in multiplication
+
+    #[test]
+    fn multiplication() {
+        // Given a left-hand side matrix and and a right-hand side matrix
+        // When the multiplication lhs * rhs is done
+        // Then the resulting matrix has the correct values
+
+        let lhs = Matrix::new(
+            3,
+            2,
+            vec![
+                //
+                1.0, 2.0, //
+                3.0, 4.0, //
+                5.0, 6.0, //
+            ],
+        );
+        let rhs = Matrix::new(
+            2,
+            3,
+            vec![
+                //
+                11.0, 12.0, 13.0, //
+                14.0, 15.0, 16.0, //
+            ],
+        );
+        let actual_product = &lhs * &rhs;
+        let expected_product = Matrix::new(
+            2,
+            2,
+            vec![
+                //
+                1.0 * 11.0 + 3.0 * 12.0 + 5.0 * 13.0,
+                2.0 * 11.0 + 4.0 * 12.0 + 6.0 * 13.0, //
+                1.0 * 14.0 + 3.0 * 15.0 + 5.0 * 16.0,
+                2.0 * 14.0 + 4.0 * 15.0 + 6.0 * 16.0, //
+            ],
+        );
+
+        assert_eq!(actual_product, expected_product);
     }
 }

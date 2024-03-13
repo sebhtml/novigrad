514c9d4e63ad3150a2a632c6f8911c9d737549eb
Sebastien Boisvert <sebhtml@protonmail.com>
2024-03-13 12:13:35 +0100

    chore: add missing file for softmax

---
+++ src/softmax.rs
@@ -0,0 +1,79 @@
+use std::f32::consts::E;
+
+use crate::{ActivationFunction, Matrix};
+
+pub struct Softmax {}
+
+impl Default for Softmax {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+impl ActivationFunction for Softmax {
+    fn activate_matrix(&self, mut product_matrix: Matrix) -> Matrix {
+        // Find max
+        let rows = product_matrix.rows();
+        let cols = product_matrix.cols();
+        let mut max = product_matrix.get(0, 0);
+        let mut row = 0;
+        while row < rows {
+            let mut col = 0;
+            while col < cols {
+                let x = product_matrix.get(row, col);
+                max = max.max(x);
+                col += 1;
+            }
+            row += 1;
+        }
+        // For each value:
+        // 1. substract the max
+        // 2. compute E^x
+        // 3. add result to sum
+        let mut sum = 0.0;
+        let mut row = 0;
+        while row < rows {
+            let mut col = 0;
+            while col < cols {
+                let x = product_matrix.get(row, col);
+                let y = E.powf(x - max);
+                product_matrix.set(row, col, y);
+                sum += y;
+                col += 1;
+            }
+            row += 1;
+        }
+
+        // Divide every value by sum.
+        let mut row = 0;
+        while row < rows {
+            let mut col = 0;
+            while col < cols {
+                let x = product_matrix.get(row, col);
+                let y = x / sum;
+                product_matrix.set(row, col, y);
+                col += 1;
+            }
+            row += 1;
+        }
+        product_matrix
+    }
+
+    fn derive_matrix(&self, mut matrix: Matrix) -> Matrix {
+        let rows = matrix.rows();
+        let cols = matrix.cols();
+        let mut row = 0;
+        while row < rows {
+            let mut col = 0;
+            while col < cols {
+                let x = matrix.get(row, col);
+                let y = x * (1.0 - x);
+                matrix.set(row, col, y);
+                col += 1;
+            }
+            row += 1;
+        }
+
+        matrix
+    }
+}

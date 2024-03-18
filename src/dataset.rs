0eafb62645aba513ed47e219b6b6663be2b13dfa
Sebastien Boisvert <sebhtml@protonmail.com>
2024-03-18 16:57:11 +0100

    refactor: uniformize input tensors

--- src/dataset.rs
+++ src/dataset.rs
@@ -1,43 +1,46 @@
 use crate::Tensor;
 
 fn add_simple_embeddings(input: Tensor) -> Tensor {
-    let table = vec![
+    let embeddings_table = vec![
         // 0.0
         vec![0.0, 0.0, 1.0, 0.0], //
         // 1.0
         vec![1.0, 0.0, 0.0, 0.0], //
     ];
     let mut values = vec![];
-    let mut row = 0;
-    let rows = input.dimensions()[0];
-    while row < rows {
-        let index = input.get(&vec![row, 0]) as usize;
-        values.append(&mut table[index].clone());
-        row += 1;
+    let mut col = 0;
+    let cols = input.dimensions()[1];
+    while col < cols {
+        let index = input.get(&vec![0, col]) as usize;
+        values.append(&mut embeddings_table[index].clone());
+        col += 1;
     }
-    Tensor::new(vec![1, input.dimensions()[0] * table[0].len(), ], values)
+    Tensor::new(
+        vec![1, input.dimensions()[0] * embeddings_table[0].len()],
+        values,
+    )
 }
 
 pub fn load_simple_examples() -> Vec<(Tensor, Tensor)> {
     let mut examples = Vec::new();
     examples.push((
         //
-        Tensor::new(vec![4, 1], vec![1.0, 0.0, 0.0, 0.0]), //
+        Tensor::new(vec![1, 4], vec![1.0, 0.0, 0.0, 0.0]), //
         Tensor::new(vec![1, 2], vec![0.1, 0.9]),
     ));
     examples.push((
         //
-        Tensor::new(vec![4, 1], vec![1.0, 0.0, 0.0, 1.0]), //
+        Tensor::new(vec![1, 4], vec![1.0, 0.0, 0.0, 1.0]), //
         Tensor::new(vec![1, 2], vec![0.1, 0.9]),
     ));
     examples.push((
         //
-        Tensor::new(vec![4, 1], vec![0.0, 0.0, 1.0, 0.0]), //
+        Tensor::new(vec![1, 4], vec![0.0, 0.0, 1.0, 0.0]), //
         Tensor::new(vec![1, 2], vec![0.9, 0.1]),
     ));
     examples.push((
         //
-        Tensor::new(vec![4, 1], vec![0.0, 1.0, 1.0, 0.0]), //
+        Tensor::new(vec![1, 4], vec![0.0, 1.0, 1.0, 0.0]), //
         Tensor::new(vec![1, 2], vec![0.9, 0.1]),
     ));
     examples

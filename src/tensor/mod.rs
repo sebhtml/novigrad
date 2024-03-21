394a4b998d318eda284efe698edb94fe8653b30c
Sebastien Boisvert <sebhtml@protonmail.com>
2024-03-21 10:55:56 +0100

    chore: remove unused method

--- src/tensor/mod.rs
+++ src/tensor/mod.rs
@@ -155,12 +155,6 @@ impl Tensor {
     }
 }
 
-impl Into<Vec<f32>> for Tensor {
-    fn into(self) -> Vec<f32> {
-        self.values
-    }
-}
-
 impl Display for Tensor {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         _ = write!(f, "Shape: {:?}", (self.rows, self.cols));

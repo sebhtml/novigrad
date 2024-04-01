f3d350315755fd1b4969aaef347bf24fb11a5297
Sebastien Boisvert <sebhtml@protonmail.com>
2024-04-01 06:23:07 +0200

    feat: add matrix multiplication with transposed lhs and transposed rhs

--- src/tensor/mod.rs
+++ src/tensor/mod.rs
@@ -207,6 +207,8 @@ impl Tensor {
             Self::matmul_lhs_t_rhs_result(lhs, rhs, result)
         } else if !tranpose_lhs && transpose_rhs && !transpose_result {
             Self::matmul_lhs_rhs_t_result(lhs, rhs, result)
+        } else if tranpose_lhs && transpose_rhs && !transpose_result {
+            Self::matmul_lhs_t_rhs_t_result(lhs, rhs, result)
         } else {
             Err(Error::UnsupportedOperation)
         }
@@ -287,6 +289,46 @@ impl Tensor {
                     inner += 1;
                 }
                 row += 1;
+            }
+        }
+
+        Ok(())
+    }
+
+    /// lhs transposed, rhs transposed, result transposed.
+    fn matmul_lhs_t_rhs_t_result(
+        lhs: &Tensor,
+        rhs: &Tensor,
+        result: &mut Tensor,
+    ) -> Result<(), Error> {
+        let lhs_rows = lhs.rows;
+        let lhs_cols = lhs.cols;
+        let rhs_rows = rhs.rows;
+        let rhs_cols = rhs.cols;
+
+        if lhs_rows != rhs_cols {
+            return Err(Error::IncompatibleTensorShapes);
+        }
+
+        result.reshape(lhs_cols, rhs_rows);
+
+        unsafe {
+            let mut lhs_col = 0;
+            while lhs_col != lhs_cols {
+                let mut rhs_row = 0;
+                while rhs_row != rhs_rows {
+                    let mut inner = 0;
+                    while inner != lhs_rows {
+                        let lhs_value = lhs.get(inner, lhs_col);
+                        let rhs_value = rhs.get(rhs_row, inner);
+                        let old = result.get(lhs_col, rhs_row);
+                        let result_value = old + lhs_value * rhs_value;
+                        result.set(lhs_col, rhs_row, result_value);
+                        inner += 1;
+                    }
+                    rhs_row += 1;
+                }
+                lhs_col += 1;
             }
         }
 
--- src/tensor/tests.rs
+++ src/tensor/tests.rs
@@ -1,6 +1,9 @@
 use rand::Rng;
 
-use crate::tensor::{Error, Tensor, TRANSPOSE_LHS, TRANSPOSE_RHS};
+use crate::{
+    tensor::{Error, Tensor, TRANSPOSE_LHS, TRANSPOSE_RHS},
+    TRANSPOSE_RESULT,
+};
 
 #[test]
 fn new() {
@@ -293,6 +296,54 @@ fn transposed_rhs_matrix_multiplication_result() {
 
     let mut result = Tensor::default();
     Tensor::matmul(&lhs, &rhs, &mut result, TRANSPOSE_RHS).expect("Ok");
+    assert_eq!(result, expected_result);
+}
+
+#[test]
+fn lhs_t_rhs_t_result_matrix_multiplication_result() {
+    // Given a left-hand side matrix and and a right-hand side matrix
+    // When the multiplication lhs * rhs is done
+    // Then the resulting matrix has the correct values
+
+    let lhs2 = Tensor::new(
+        3,
+        2,
+        vec![
+            1.0, 2.0, //
+            3.0, 4.0, //
+            5.0, 6.0, //
+        ],
+    );
+    let mut lhs = Tensor::default();
+    lhs2.transpose(&mut lhs);
+    let rhs2 = Tensor::new(
+        2,
+        3,
+        vec![
+            11.0, 12.0, 13.0, //
+            14.0, 15.0, 16.0, //
+        ],
+    );
+    let mut rhs = Tensor::default();
+    rhs2.transpose(&mut rhs);
+    let expected_result = Tensor::new(
+        3,
+        3,
+        vec![
+            1.0 * 11.0 + 2.0 * 14.0,
+            1.0 * 12.0 + 2.0 * 15.0,
+            1.0 * 13.0 + 2.0 * 16.0, //
+            3.0 * 11.0 + 4.0 * 14.0,
+            3.0 * 12.0 + 4.0 * 15.0,
+            3.0 * 13.0 + 4.0 * 16.0, //
+            5.0 * 11.0 + 6.0 * 14.0,
+            5.0 * 12.0 + 6.0 * 15.0,
+            5.0 * 13.0 + 6.0 * 16.0, //
+        ],
+    );
+
+    let mut result = Tensor::default();
+    Tensor::matmul(&lhs, &rhs, &mut result, TRANSPOSE_LHS | TRANSPOSE_RHS).expect("Ok");
     assert_eq!(result, expected_result);
 }
 

abe0c4b08778a469fd2e454c49c7c104731ed6e1
Sebastien Boisvert <sebhtml@protonmail.com>
2024-04-10 05:32:18 -0400

    test: add a gemm row major test

--- src/gemm/tests.rs
+++ src/gemm/tests.rs
@@ -1,6 +1,6 @@
 /// This is the example from https://docs.rs/cblas/latest/cblas/.
 #[test]
-fn sgemm() {
+fn sgemm_column_major() {
     use cblas::*;
 
     let (m, n, k) = (2, 4, 3);
@@ -44,5 +44,65 @@ fn sgemm() {
         );
     }
 
-    assert!(c == vec![40.0, 90.0, 50.0, 100.0, 50.0, 120.0, 60.0, 130.0,]);
+    assert_eq!(
+        c,
+        vec![
+            //
+            40.0, 90.0, //
+            50.0, 100.0, //
+            50.0, 120.0, //
+            60.0, 130.0, //
+        ]
+    );
+}
+
+#[test]
+fn sgemm_row_major() {
+    use cblas::*;
+
+    let (m, n, k) = (2, 4, 3);
+    let a = vec![
+        //
+        1.0, 2.0, 3.0, //
+        4.0, 5.0, 6.0, //
+    ];
+    let b = vec![
+        //
+        1.0, 2.0, 3.0, 4.0, //
+        5.0, 6.0, 7.0, 8.0, //
+        9.0, 10.0, 11.0, 12.0, //
+    ];
+    let mut c = vec![
+        //
+        2.0, 6.0, 0.0, 4.0, //
+        7.0, 2.0, 7.0, 2.0, //
+    ];
+
+    unsafe {
+        sgemm(
+            Layout::RowMajor,
+            Transpose::None,
+            Transpose::None,
+            m,
+            n,
+            k,
+            1.0,
+            &a,
+            k,
+            &b,
+            n,
+            1.0,
+            &mut c,
+            n,
+        );
+    }
+
+    assert_eq!(
+        c,
+        vec![
+            //
+            40.0, 50.0, 50.0, 60.0, //
+            90.0, 100.0, 120.0, 130.0, //
+        ]
+    );
 }

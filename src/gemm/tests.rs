8d089d25f90c12f2e6a6216db36d5c6ebc478636
Sebastien Boisvert <sebhtml@protonmail.com>
2024-04-10 06:25:01 -0400

    test: add a gemm test

--- src/gemm/tests.rs
+++ src/gemm/tests.rs
@@ -106,3 +106,56 @@ fn sgemm_row_major() {
         ]
     );
 }
+
+#[test]
+fn sgemm_row_major_a_transpose() {
+    use cblas::*;
+
+    let (m, n, k) = (2, 4, 3);
+    let lda = m;
+    let a = vec![
+        //
+        1.0, 4.0, //
+        2.0, 5.0, //
+        3.0, 6.0, //
+    ];
+    let b = vec![
+        //
+        1.0, 2.0, 3.0, 4.0, //
+        5.0, 6.0, 7.0, 8.0, //
+        9.0, 10.0, 11.0, 12.0, //
+    ];
+    let mut c = vec![
+        //
+        2.0, 6.0, 0.0, 4.0, //
+        7.0, 2.0, 7.0, 2.0, //
+    ];
+
+    unsafe {
+        sgemm(
+            Layout::RowMajor,
+            Transpose::Ordinary,
+            Transpose::None,
+            m,
+            n,
+            k,
+            1.0,
+            &a,
+            lda,
+            &b,
+            n,
+            1.0,
+            &mut c,
+            n,
+        );
+    }
+
+    assert_eq!(
+        c,
+        vec![
+            //
+            40.0, 50.0, 50.0, 60.0, //
+            90.0, 100.0, 120.0, 130.0, //
+        ]
+    );
+}

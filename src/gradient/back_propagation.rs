deded041a94be2b852290c31db7e5a02415e2eaa
Sebastien Boisvert <sebhtml@protonmail.com>
2024-04-20 17:43:28 -0400

    refactor: simplify back-propagation

--- src/gradient/back_propagation.rs
+++ src/gradient/back_propagation.rs
@@ -1,4 +1,3 @@
-use std::mem::swap;
 use std::{cell::RefCell, ops::Deref, rc::Rc};
 
 use crate::{Accelerator, DeltaWorkingMemory, Error, Gradient, Record, Tape, TrainWorkingMemory};
@@ -13,7 +12,6 @@ pub fn back_propagation(
     tape: &Rc<RefCell<Tape>>,
 ) -> Result<Vec<Gradient>, Error> {
     let mut gradients = vec![];
-    let next_layer_delta = &mut working_memory.next_layer_delta;
     let layer_delta = &mut working_memory.layer_delta;
     let tape: &Tape = &tape.deref().borrow();
     let records: &Vec<Record> = &tape.records();
@@ -21,28 +19,13 @@ pub fn back_propagation(
     let tmp = &mut working_memory.tmp;
 
     let back_propagated_delta = &mut working_memory.back_propagated_delta;
-    next_layer_delta.reset(1, 333, 0.0);
-    back_propagated_delta.reset(1, 444, 0.0);
+    back_propagated_delta.reset(0, 0, Default::default());
 
     for layer_index in (0..layers_count).into_iter().rev() {
         let record = &records[layer_index];
         let inputs = record.inputs();
         let output = record.output();
         let operator: &Box<dyn OperatorTrait> = &record.operator().deref().borrow();
-
-        if layer_index != layers_count - 1 {
-            let next_layer_index = layer_index + 1;
-            let next_record = &records[next_layer_index];
-            let next_operator: &Box<dyn OperatorTrait> = &next_record.operator().deref().borrow();
-            let next_inputs = next_record.inputs();
-
-            next_operator.backward(
-                next_inputs,
-                accelerator,
-                next_layer_delta,
-                back_propagated_delta,
-            );
-        }
 
         operator.get_layer_output_delta(
             accelerator,
@@ -52,7 +35,6 @@ pub fn back_propagation(
             back_propagated_delta,
             tmp,
         );
-
         tmp.clip(-1.0, 1.0, layer_delta);
 
         let mut operator_gradients =
@@ -63,7 +45,7 @@ pub fn back_propagation(
             gradients.append(&mut operator_gradients);
         }
 
-        swap(next_layer_delta, layer_delta);
+        operator.backward(inputs, accelerator, layer_delta, back_propagated_delta);
     }
     Ok(gradients)
 }

ca4ecdb81c4ebd1d253056d5907a97030f0c2ac5
Sebastien Boisvert <sebhtml@protonmail.com>
2024-04-20 15:14:04 -0400

    refactor: use less code for back propagation

--- src/gradient/back_propagation.rs
+++ src/gradient/back_propagation.rs
@@ -1,9 +1,7 @@
 use std::mem::swap;
 use std::{cell::RefCell, ops::Deref, rc::Rc};
 
-use crate::{
-    Accelerator, DeltaWorkingMemory, Error, Gradient, Record, Tape, Tensor, TrainWorkingMemory,
-};
+use crate::{Accelerator, DeltaWorkingMemory, Error, Gradient, Record, Tape, TrainWorkingMemory};
 
 use crate::gradient::OperatorTrait;
 
@@ -21,24 +19,26 @@ pub fn back_propagation(
     let records: &Vec<Record> = &tape.records();
     let layers_count = { tape.records().len() };
     let tmp = &mut working_memory.tmp;
+
     let back_propagated_delta = &mut working_memory.back_propagated_delta;
     next_layer_delta.assign(accelerator, &Default::default());
+
     for layer_index in (0..layers_count).into_iter().rev() {
         let record = &records[layer_index];
         let inputs = record.inputs();
         let output = record.output();
         let operator: &Box<dyn OperatorTrait> = &record.operator().deref().borrow();
+
         if layer_index == layers_count - 1 {
             back_propagated_delta.assign(accelerator, next_layer_delta);
         } else {
             let next_layer_index = layer_index + 1;
-            let next_operator: &Box<dyn OperatorTrait> =
-                &records[next_layer_index].operator().deref().borrow();
-            let next_inputs: &Vec<Rc<Tensor>> = { &tape.records()[layer_index + 1].inputs() };
+            let next_record = &records[next_layer_index];
+            let next_operator: &Box<dyn OperatorTrait> = &next_record.operator().deref().borrow();
+            let next_inputs = next_record.inputs();
 
-            let next_layer = next_operator.deref();
-            next_layer.backward(
-                &next_inputs,
+            next_operator.backward(
+                next_inputs,
                 accelerator,
                 next_layer_delta,
                 back_propagated_delta,
@@ -48,8 +48,8 @@ pub fn back_propagation(
         operator.get_layer_output_delta(
             accelerator,
             error_working_memory,
-            &inputs,
-            &output,
+            inputs,
+            output,
             back_propagated_delta,
             tmp,
         );

7768e360758ac54157247716c6926c3d0fcfb886
Sebastien Boisvert <sebhtml@protonmail.com>
2024-05-17 14:58:39 -0400

    feat: add Gemm opcode

--- src/neural_machine/opcode.rs
+++ src/neural_machine/opcode.rs
@@ -1,26 +1,27 @@
 use std::rc::Rc;
 
-use crate::Operator;
+use crate::{Error, Gemm, Operator, TensorF32};
 
 #[derive(Clone, Debug)]
 pub enum OpCode {
     DynOperator(Rc<dyn Operator>),
+    Gemm(bool, bool, bool),
 }
 
 impl Operator for OpCode {
     fn name(&self) -> &str {
         match self {
             OpCode::DynOperator(inner) => inner.name(),
+            OpCode::Gemm(_, _, _) => "Gemm",
         }
     }
 
-    fn forward(
-        &self,
-        inputs: &[&crate::TensorF32],
-        outputs: &[&crate::TensorF32],
-    ) -> Result<(), crate::Error> {
+    fn forward(&self, inputs: &[&TensorF32], outputs: &[&TensorF32]) -> Result<(), Error> {
         match self {
             OpCode::DynOperator(inner) => inner.forward(inputs, outputs),
+            OpCode::Gemm(trans_a, trans_b, trans_result) => {
+                Gemm::execute(*trans_a, *trans_b, *trans_result, inputs, outputs)
+            }
         }
     }
 }
--- src/operators/lin_alg/gemm.rs
+++ src/operators/lin_alg/gemm.rs
@@ -3,18 +3,39 @@ use crate::{devices::Device, Error, Operator, TensorF32};
 /// https://onnx.ai/onnx/operators/onnx__Gemm.html
 #[derive(Clone)]
 pub struct Gemm {
-    transa: bool,
-    transb: bool,
-    transpose_result: bool,
+    trans_a: bool,
+    trans_b: bool,
+    trans_result: bool,
 }
 
 impl Gemm {
     pub fn new(_device: &Device, transa: bool, transb: bool, transpose_result: bool) -> Self {
         Self {
-            transa,
-            transb,
-            transpose_result,
+            trans_a: transa,
+            trans_b: transb,
+            trans_result: transpose_result,
         }
+    }
+
+    pub fn execute(
+        trans_a: bool,
+        trans_b: bool,
+        trans_result: bool,
+        inputs: &[&crate::TensorF32],
+        outputs: &[&crate::TensorF32],
+    ) -> Result<(), Error> {
+        debug_assert_eq!(inputs.len(), 2);
+        debug_assert_eq!(outputs.len(), 1);
+        let input = inputs[0];
+        let weights = inputs[1];
+        let biases = outputs[0];
+        let a = input;
+        let b = weights;
+        let c = biases;
+        let transa = trans_a;
+        let transb = trans_b;
+        let transpose_result = trans_result;
+        TensorF32::gemm(transa, transb, 1.0, a, b, 1.0, c, transpose_result)
     }
 }
 
@@ -28,17 +49,12 @@ impl Operator for Gemm {
         inputs: &[&crate::TensorF32],
         outputs: &[&crate::TensorF32],
     ) -> Result<(), Error> {
-        debug_assert_eq!(inputs.len(), 2);
-        debug_assert_eq!(outputs.len(), 1);
-        let input = inputs[0];
-        let weights = inputs[1];
-        let biases = outputs[0];
-        let a = input;
-        let b = weights;
-        let c = biases;
-        let transa = self.transa;
-        let transb = self.transb;
-        let transpose_result = self.transpose_result;
-        TensorF32::gemm(transa, transb, 1.0, a, b, 1.0, c, transpose_result)
+        Self::execute(
+            self.trans_a,
+            self.trans_b,
+            self.trans_result,
+            inputs,
+            outputs,
+        )
     }
 }

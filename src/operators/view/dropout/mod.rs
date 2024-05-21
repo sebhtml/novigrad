4b12d4000d000b301a77f2d76a28c3613fbb51d7
Sebastien Boisvert <sebhtml@protonmail.com>
2024-05-21 15:41:49 -0400

    feat: implement Dropout

--- src/operators/view/dropout/mod.rs
+++ src/operators/view/dropout/mod.rs
@@ -1,4 +1,11 @@
-use crate::{BinaryOperator, Device, Error, Mul, Tensor, UnaryOperator};
+use std::ops::Deref;
+
+use rand::{distributions::Uniform, thread_rng, Rng};
+
+use crate::{
+    inference_instruction, BinaryOperator, Device, Error, GenericTensor, Mul, OpCode, ScalarMul,
+    Tensor, UnaryOperator,
+};
 
 #[cfg(test)]
 mod tests;
@@ -6,7 +13,8 @@ mod tests;
 pub struct Dropout {
     mask: Tensor,
     mul: Mul,
-    _p: f32,
+    scalar_mul: ScalarMul,
+    dropout_probability: f32,
 }
 
 impl Dropout {
@@ -14,26 +22,56 @@ impl Dropout {
         device: &Device,
         mask_rows: usize,
         mask_cols: usize,
-        p: f32,
+        dropout_probability: f32,
     ) -> Result<Self, Error> {
         let len = mask_rows * mask_cols;
         let mask = vec![1.0; len];
-        let mask = device.tensor(mask_rows, mask_cols, mask, &[], true, true);
+        let mask = device.tensor(mask_rows, mask_cols, mask, &[], false, false);
         let mul = Mul::new(device);
-        let mask = Self { mask, mul, _p: p };
+        let alpha = 1.0 / (1.0 - dropout_probability);
+        let scalar_mul = ScalarMul::new(device, alpha);
+        let mask = Self {
+            mask,
+            mul,
+            scalar_mul,
+            dropout_probability,
+        };
         Ok(mask)
+    }
+
+    pub fn execute(
+        dropout_probability: f32,
+        inputs: &[&GenericTensor],
+        outputs: &[&GenericTensor],
+    ) -> Result<(), Error> {
+        // zero each element of the mask with probability p.
+        let input = inputs[0];
+        let output = outputs[0];
+        let len = input.len();
+        let mut values = vec![1.0; len];
+        let mut rng = thread_rng();
+        let uniform = Uniform::new(0.0, 1.0);
+
+        for i in 0..len {
+            if rng.sample(uniform) < dropout_probability {
+                values[i] = 0.0;
+            }
+        }
+        output.set_values(values);
+        Ok(())
     }
 }
 
 impl UnaryOperator for Dropout {
     fn forward(&self, input: &Tensor) -> Result<Tensor, Error> {
-        // TODO zero each element of the mask with probability p.
-        /*
-        let mut values = mask.tensor().deref().borrow().get_values()?;
-        let mut indices = (0..values.len()).collect()
-        mask.tensor().deref().borrow_mut().set_values(values);
-         */
-        self.mul.forward(input, &self.mask)
-        // TODO scale my 1 ( 1 - p )
+        let mask = &self.mask;
+        mask.push_instruction(inference_instruction!(
+            OpCode::Dropout(self.dropout_probability),
+            &[&mask.tensor().deref().borrow()],
+            &[&mask.tensor().deref().borrow()],
+        ));
+        let mul_output = self.mul.forward(input, &self.mask)?;
+        let scalar_mul_output = self.scalar_mul.forward(&mul_output)?;
+        Ok(scalar_mul_output)
     }
 }

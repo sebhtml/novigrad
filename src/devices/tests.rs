af79957d653f0b75849779c000dcfe807d537fbc
Sebastien Boisvert <sebhtml@protonmail.com>
2024-05-26 06:39:56 -0400

    test: add clip max test

--- src/devices/tests.rs
+++ src/devices/tests.rs
@@ -37,3 +37,43 @@ fn clip_min() {
         .unwrap();
     assert_eq!(expected.get_values(), output.get_values(),);
 }
+
+#[test]
+fn clip_max() {
+    use crate::devices::DeviceInterface;
+    use crate::Device;
+    let device = Device::default();
+    let input = device
+        .tensor(
+            2,
+            3,
+            vec![
+                //
+                -1.0, 4.0, //
+                2.0, 5.0, //
+                3.0, -6.0, //
+            ],
+        )
+        .unwrap();
+    let output = device.tensor(2, 3, vec![0.0; 6]).unwrap();
+
+    let min = device.tensor(1, 1, vec![f32::NEG_INFINITY]).unwrap();
+
+    let max = device.tensor(1, 1, vec![2.0]).unwrap();
+
+    device.clip(&min, &max, &input, &output).unwrap();
+
+    let expected = device
+        .tensor(
+            2,
+            3,
+            vec![
+                //
+                -1.0, 2.0, //
+                2.0, 2.0, //
+                2.0, -6.0, //
+            ],
+        )
+        .unwrap();
+    assert_eq!(expected.get_values(), output.get_values(),);
+}

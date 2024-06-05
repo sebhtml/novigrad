80b0a6f6698d5412b482976876586a7eb0615801
Sebastien Boisvert <sebhtml@protonmail.com>
2024-06-05 06:21:43 -0400

    feat: implement 'partition_branching_graph'

--- src/graph_theory/mod.rs
+++ src/graph_theory/mod.rs
@@ -2,48 +2,67 @@
 mod tests;
 
 #[allow(unused)]
-pub fn find_vertex_disjoint_paths(vertices: &[usize], edges: &[(usize, usize)]) -> Vec<Vec<usize>> {
-    println!("vertices {:?}", vertices);
-    println!("edges {:?}", edges);
+pub fn partition_branching_graph(vertices: &[usize], edges: &[(usize, usize)]) -> Vec<Vec<(usize, usize)>> {
     let mut dependencies = vec![Vec::<usize>::default(); vertices.len()];
     let mut dependents = vec![Vec::<usize>::default(); vertices.len()];
     for (dependency, dependent) in edges.iter() {
         dependencies[*dependent].push(*dependency);
         dependents[*dependency].push(*dependent);
     }
-    let mut paths = vec![];
-    let mut in_path = vec![false; vertices.len()];
-    for v1 in 0..vertices.len() {
-        for v2 in 0..vertices.len() {
-            if v1 == v2 {
+
+    // Step 1: Find all pairs of vertices (u, v)
+    let mut uv_pairs = vec![];
+
+    for u in 0..vertices.len() {
+        for v in 0..vertices.len() {
+            if u == v {
                 continue;
             }
 
-            let v1_dependents = &dependents[v1];
-            let v2_dependencies = &dependencies[v2];
+            let u_dependents = &dependents[u];
+            let v_dependencies = &dependencies[v];
 
-            if v1_dependents.len() >= 2 && v1_dependents.len() == v2_dependencies.len() {
-                for dependent in v1_dependents.iter() {
-                    paths.push(vec![*dependent]);
-                    in_path[*dependent] = true;
-                }
+            if u_dependents.len() >= 2 && u_dependents.len() == v_dependencies.len() {
+                uv_pairs.push(((u, v), u_dependents.len()));
             }
         }
     }
 
-    for (dependency, dependent) in edges.iter() {
-        if !in_path[*dependency] && !in_path[*dependent] {
-            paths.push(vec![*dependency, *dependent]);
-            in_path[*dependency] = true;
-            in_path[*dependent] = true;
+    let mut partitions = vec![];
+
+    // For each pair (u, v)
+    for ((u, v), n) in uv_pairs.iter() {
+        // With pair (u1, v1) as the next pair, if any
+        // Get { all ascendents of u } ∪ { u }
+        // For each w in dependants of u
+        //   With z as the dependency of v that is a descendent of w
+        //   Get { w } ∪ ( { all descendents of w } ∩ { all ascendents of z } ) ∪ { z }
+        // Get { v } ∪ ( { all descendents of v } ∩ { all ascendents of u2 } )
+        
+        let edge_u = edges.iter().enumerate()
+            .find(|(i, (a, b))| b == u).map(|(i, _)| i).unwrap();
+        let edge_v = edges.iter().enumerate()
+            .find(|(i, (a, b))| a == v).map(|(i, _)| i).unwrap();
+
+        println!("(u, v) is ({}, {})", u, v);
+        println!("edge_u {}", edge_u);
+        println!("edge_v {}", edge_v);
+
+        let part_u = edges[0..(edge_u + 1)].to_owned();
+        partitions.push(part_u);
+        let uv_parts_begin = edge_u + 1;
+        let uv_parts_end = edge_v;
+        let uv_edges = uv_parts_end - uv_parts_begin;
+        let uv_edges_per_uv_part = uv_edges / n;
+        for i in 0..*n {
+            let part_uv_begin = uv_parts_begin + i * uv_edges_per_uv_part;
+            let part_uv_end = part_uv_begin + uv_edges_per_uv_part;
+            let part_uv_i = edges[part_uv_begin..part_uv_end].to_owned();
+            partitions.push(part_uv_i);
         }
+        let part_v = edges[edge_v..edges.len()].to_owned();
+        partitions.push(part_v);
     }
 
-    for vertex in vertices.iter() {
-        if !in_path[*vertex] {
-            paths.push(vec![*vertex]);
-            in_path[*vertex] = true;
-        }
-    }
-    paths
+    partitions
 }
--- src/graph_theory/tests.rs
+++ src/graph_theory/tests.rs
@@ -1,32 +1,43 @@
-use crate::graph_theory::find_vertex_disjoint_paths;
+use crate::graph_theory::partition_branching_graph;
 
 #[test]
-fn problem_4_paths() {
+fn problem_6_partitions() {
+    /*
+    0
+    1
+ 2 3 4 5
+    6
+    7
+     */
     let edges = vec![
         (0, 1),
         (1, 2),
-        (1, 3),
-        (1, 4),
-        (1, 5),
         (2, 6),
+        (1, 3),
         (3, 6),
+        (1, 4),
         (4, 6),
+        (1, 5),
         (5, 6),
         (6, 7),
     ];
 
     let vertices = (0..8).collect::<Vec<_>>();
 
-    let paths = find_vertex_disjoint_paths(&vertices, &edges);
+   
+    let paths = partition_branching_graph(&vertices, &edges);
 
+    println!("vertices {:?}", vertices);
+    println!("edges {:?}", edges);
     println!("paths: {:?}", paths);
+
     assert_eq!(6, paths.len());
-    assert_eq!(vec![2], paths[0]);
-    assert_eq!(vec![3], paths[1]);
-    assert_eq!(vec![4], paths[2]);
-    assert_eq!(vec![5], paths[3]);
-    assert_eq!(vec![0, 1], paths[4]);
-    assert_eq!(vec![6, 7], paths[5]);
+    assert_eq!(vec![(0, 1)], paths[0]);
+    assert_eq!(vec![(1, 2), (2, 6)], paths[1]);
+    assert_eq!(vec![(1, 3), (3, 6)], paths[2]);
+    assert_eq!(vec![(1, 4), (4, 6)], paths[3]);
+    assert_eq!(vec![(1, 5), (5, 6)], paths[4]);
+    assert_eq!(vec![(6, 7)], paths[5]);
 }
 
 #[test]
@@ -49,14 +60,9 @@ fn problem_2_with_4_paths() {
 
     let vertices = (0..11).collect::<Vec<_>>();
 
-    let paths = find_vertex_disjoint_paths(&vertices, &edges);
+    let paths = partition_branching_graph(&vertices, &edges);
 
     println!("paths: {:?}", paths);
     assert_eq!(6, paths.len());
-    assert_eq!(vec![2], paths[0]);
-    assert_eq!(vec![3], paths[1]);
-    assert_eq!(vec![4], paths[2]);
-    assert_eq!(vec![5], paths[3]);
-    assert_eq!(vec![0, 1], paths[4]);
-    assert_eq!(vec![6, 7], paths[5]);
+
 }

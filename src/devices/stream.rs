45e71a99061c2b64ae1c8d981d360691e4b00606
Sebastien Boisvert <sebhtml@protonmail.com>
2024-06-14 06:41:09 -0400

    feat: add synchronize method for stream

--- src/devices/stream.rs
+++ src/devices/stream.rs
@@ -1,3 +1,5 @@
+use crate::tensor::Error;
+
 #[cfg(feature = "cuda")]
 use super::cuda::stream::CudaDeviceStream;
 
@@ -5,4 +7,18 @@ pub enum DeviceStream {
     CpuDeviceStream,
     #[cfg(feature = "cuda")]
     CudaDeviceStream(CudaDeviceStream),
+}
+
+pub trait StreamTrait {
+    fn synchronize(&self) -> Result<(), Error>;
+}
+
+impl StreamTrait for DeviceStream {
+    fn synchronize(&self) -> Result<(), Error> {
+        match self {
+            DeviceStream::CpuDeviceStream => Ok(()),
+            #[cfg(feature = "cuda")]
+            DeviceStream::CudaDeviceStream(stream) => stream.synchronize(),
+        }
+    }
 }
